2cbc07c291f5ef55b508655cefad9953dd80beea
akmen <akmen.news@gmail.com>
2021-06-09 13:15:59 +0300

    Updete the while loop - with a call to update_screen()

--- Alien Invasion/alien_invasion.py
+++ Alien Invasion/alien_invasion.py
@@ -34,30 +34,12 @@ def run_game():
     # The game is controlled by a while loop
     # that contains an event loop and code that manages screen updates
     while True:
+        # These two functions make the while loop simpler and will make further
+        # development easier. Instead of working inside run_game(), we can do most
+        # of our work in the module game_functions.
         gf.chek_events()
+        gf.update_screen(ai_settings, screen, ship)
 
-        # Redraw the screen during each pass through the loop
-        # Fill the screen with the background color using the screen.fill() method,
-        # which takes only one argument: a color.
-        # access the background color when filling the screen at as well.
-
-        screen.fill(ai_settings.bg_color)
-
-        # We draw the ship onscreen by calling ship.blitme() after filling the
-        # background, so the ship appears on top of the background.
-
-        ship.blitme()
-
-
-
-        # Make the most recently drawn screen visible.
-
-        # The call to pygame.display.flip() at tells Pygame to make the most recently drawn
-        # screen visible. In this case it draws an empty screen each time through the while loop
-        # to erase the old screen so that only the new screen is visible. When we move the game
-        # elements around, pygame.display.flip() will continually update the display to show the
-        # new positions of elements and hide the old ones, creating the illusion of smooth movement.
-        pygame.display.flip()
 
 
 # which initializes the game and starts the main loop.
