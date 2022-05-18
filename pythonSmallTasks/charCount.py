d2a31bce90f627e7d798fb06bb6abd627283efa6
akmen <akmen.news@gmail.com>
2022-05-18 12:14:20 +0200

    char Count

--- pythonSmallTasks/charCount.py
+++ pythonSmallTasks/charCount.py
@@ -1,7 +1,11 @@
+import pprint
 massage = ' It was a bright cold day in Aprel, and the clocks were striking thirteen.'
 count = {}
 
 for character in massage:
     count.setdefault(character, 0)
     count[character] = count[character] + 1
+
 print(count)
+
+pprint.pprint(count)
